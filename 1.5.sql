1e02659a44b166fa7b56ab2c7c88999fbeca0578
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:35:30 +0200

    Update 1.5.sql

--- 1.5.sql
+++ 1.5.sql
@@ -1,1 +1,3 @@
+// Erstelle eine Top-3 Liste der Filme mit den meisten Schauspielerinnen und Schauspielern.
+// Sortiere entsprechend. 
 SELECT title, COUNT(name) AS anzahl FROM movie, actor WHERE mid=movie_id GROUP BY title ORDER BY anzahl DESC FETCH FIRST 3 ROWS ONLY;
