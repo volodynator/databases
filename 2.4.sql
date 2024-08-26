8690a40e1797104a343e9679dc4ae3a63bbb0192
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:38:05 +0200

    Update 2.4.sql

--- 2.4.sql
+++ 2.4.sql
@@ -1,1 +1,5 @@
+// Formuliere eine Anfrage, die die Jahreszahl und die Anzahl der in diesem Jahr veröffentlichten
+// Filme abfragt für
+// • das höchste (späteste) vorkommende Jahr und
+// • das Jahr mit den meisten veröffentlichten Filmen
 ((SELECT year, COUNT(mid) AS anzahl FROM movie GROUP BY year ORDER BY year DESC FETCH FIRST 1 ROW ONLY) UNION (SELECT year, COUNT(mid) AS anzahl FROM movie GROUP BY year ORDER BY anzahl DESC FETCH FIRST 1 ROW ONLY)) ORDER BY year DESC;
