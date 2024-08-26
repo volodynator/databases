c1d9bb4d89e174781c55cf7aa731c6e155998857
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:34:17 +0200

    Update 1.2.sql

--- 1.2.sql
+++ 1.2.sql
@@ -1,1 +1,4 @@
+// Gib alle Produzenten aus der Tabelle producer aus, die keine zugehörigen Einträge in der
+// Filmtabelle movie haben. Das Ergebnis soll aufsteigend sortiert sein und jeden Produzenten
+// nur einmal enthalten. 
 SELECT DISTINCT name FROM producer LEFT JOIN movie ON movie_id=mid WHERE mid is NULL ORDER BY name ASC;
