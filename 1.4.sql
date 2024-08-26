8e722dbcd408c228c6bccde88594f72d85bc8d8d
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:34:58 +0200

    Update 1.4.sql

--- 1.4.sql
+++ 1.4.sql
@@ -1,1 +1,3 @@
+// Gib die Namen der Schauspieler und Produzenten an, die an der Serie “Edge of Night, The“
+// beteiligt waren.
 (SELECT name FROM actor, movie WHERE title = 'Edge of Night, The' AND movie_id = mid) UNION (SELECT name FROM producer, movie WHERE title = 'Edge of Night, The' AND movie_id = mid);
