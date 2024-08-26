8d06f8815f132dfabd9d03eda522af6b89639d0e
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:35:57 +0200

    Update 1.6.sql

--- 1.6.sql
+++ 1.6.sql
@@ -1,1 +1,3 @@
+// Erstelle eine Top-3 Liste der Schauspieler und Schauspielerinnen mit den meisten Filmen.
+// Sortiere entsprechend.
 SELECT name, COUNT(title) AS anzahl FROM ((SELECT name, movie_id FROM actor) UNION (SELECT name, movie_id FROM actress)), movie WHERE movie_id=mid GROUP BY name ORDER BY anzahl DESC FETCH FIRST 3 ROWS ONLY;
