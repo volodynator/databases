11778281a7264b33a89cfd322fd0fe7a682d7ba0
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:37:31 +0200

    Update 2.3.sql

--- 2.3.sql
+++ 2.3.sql
@@ -1,1 +1,3 @@
+// Gib die Namen aller Producer an, die im Jahr 2001 Filme in beliebten Genres gedreht haben.
+// Ein „beliebtes Genre“ sei ein Genre in dem mindestens 200 Filme gedreht wurden.
 SELECT name FROM producer, (SELECT mid FROM movie, genre WHERE mid=movie_id AND year = 2001 AND genre IN (SELECT genre FROM genre GROUP BY genre HAVING COUNT(movie_id) >= 200)) WHERE movie_id=mid;
