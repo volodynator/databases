364edeea01dcf0f4a97ff73a8c37399df34bf827
Vladimir Simakov <68737141+volodynator@users.noreply.github.com>
2024-08-26 21:36:25 +0200

    Update 2.1.sql

--- 2.1.sql
+++ 2.1.sql
@@ -1,1 +1,3 @@
+// Gib alle Schauspieler an, die in mindestens einem Film des Genres „Action“ spielen und
+// deren Name mit „Tu“ beginnt; sortiere aufsteigend.
 SELECT DISTINCT name FROM actor, (SELECT movie_id FROM genre, movie WHERE movie_id=mid AND genre ='Action') WHERE name LIKE 'Tu%' ORDER BY name ASC;
